5a63e032fd3e8e88bb63bf3f87e701148631deb8
tyna <vojtiskova.kristyna@gmail.com>
2023-01-28 10:42:48 +0100

    a vyreseno

---
+++ 15/15.py
@@ -0,0 +1,46 @@
+
+
+from dijkstar import Graph, find_path
+import numpy as np
+
+with open('15_input.txt', encoding = 'utf-8-sig') as txt:
+    lines = txt.readlines()
+    lines = [x.strip() for x in lines]
+    lines = [list(i) for i in lines]
+    lines = np.array(lines)
+    lines = lines.astype('int64')
+    lines_width = np.shape(lines)[0]
+    lines_length = np.shape(lines)[1]
+
+
+class Position:
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
+        self.risk_level = lines[self.coordinates]
+
+    def __repr__(self):
+        return str(self.risk_level)
+
+    def get_adjacent(self):
+        self.row = self.coordinates[0]
+        self.col = self.coordinates[1]
+        adjacent_coordinates = [(self.row - 1, self.col), (self.row + 1, self.col), (self.row, self.col + 1), (self.row, self.col - 1)]
+        adjacent_coordinates = [row_col_tuple for row_col_tuple in adjacent_coordinates if (row_col_tuple[0] in range(lines_width) and row_col_tuple[1] in range(lines_length))]
+        return adjacent_coordinates
+
+def instantiate_positions(input_lines = lines):
+    coordinates_combinations = []
+    for i in range(len(input_lines)):
+        for ii in range(len(input_lines[0])):
+            coordinates_combinations.append((i, ii))
+    all_positions = {coordinates: Position(coordinates) for coordinates in coordinates_combinations}
+    return all_positions
+all_positions = instantiate_positions()
+
+graph = Graph()
+for coordinates, position in all_positions.items():
+    for adjacent in position.get_adjacent():
+        adjacent = all_positions[adjacent]
+        graph.add_edge(coordinates, adjacent.coordinates, adjacent.risk_level)
+print(find_path(graph, (0,0), (lines_width - 1,lines_length - 1)))
+
